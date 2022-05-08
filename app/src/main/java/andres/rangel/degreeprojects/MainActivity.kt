9f92161fb4e222c8ade749ae72ba912b23041694
AndresRangelMeli <andres.rangel@mercadolibre.com.co>
2022-05-08 07:39:12 -0500

    Initial Commit

---
+++ app/src/main/java/andres/rangel/degreeprojects/MainActivity.kt
@@ -0,0 +1,11 @@
+package andres.rangel.degreeprojects
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
